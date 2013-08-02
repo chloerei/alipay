d93792b6db817eb1493fef00fd97b3835cb26b68
Rei <chloerei@gmail.com>
2013-08-02 15:22:11 +0800

    Verify sign when verify notify.

--- lib/alipay/notify.rb
+++ lib/alipay/notify.rb
@@ -1,8 +1,12 @@
 module Alipay
   class Notify
     def self.verify?(params)
-      params = Utils.symbolize_keys(params)
-      open("http://notify.alipay.com/trade/notify_query.do?partner=#{Alipay.pid}&notify_id=#{CGI.escape params[:notify_id].to_s}").read == 'true'
+      if Sign.verify?(params)
+        params = Utils.symbolize_keys(params)
+        open("http://notify.alipay.com/trade/notify_query.do?partner=#{Alipay.pid}&notify_id=#{CGI.escape params[:notify_id].to_s}").read == 'true'
+      else
+        false
+      end
     end
   end
 end
--- test/alipay/notify_test.rb
+++ test/alipay/notify_test.rb
@@ -1,13 +1,20 @@
 require 'test_helper'
 
 class Alipay::NotifyTest < Test::Unit::TestCase
+  def setup
+    @options = {
+      :notify_id => '1234'
+    }
+    @options.merge!(:sign_type => 'MD5', :sign => Alipay::Sign.generate(@options))
+  end
+
   def test_verify_notify_when_true
-    FakeWeb.register_uri(:get, "http://notify.alipay.com/trade/notify_query.do?partner=#{Alipay.pid}&notify_id=true_id", :body => "true")
-    assert Alipay::Notify.verify?(:notify_id => 'true_id')
+    FakeWeb.register_uri(:get, "http://notify.alipay.com/trade/notify_query.do?partner=#{Alipay.pid}&notify_id=1234", :body => "true")
+    assert Alipay::Notify.verify?(@options)
   end
 
   def test_verify_notify_when_false
-    FakeWeb.register_uri(:get, "http://notify.alipay.com/trade/notify_query.do?partner=#{Alipay.pid}&notify_id=fake_id", :body => "false")
-    assert !Alipay::Notify.verify?(:notify_id => 'fake_id')
+    FakeWeb.register_uri(:get, "http://notify.alipay.com/trade/notify_query.do?partner=#{Alipay.pid}&notify_id=1234", :body => "false")
+    assert !Alipay::Notify.verify?(@options)
   end
 end
