759a82bd7ace94457df7873a70885dfd2bce6aca
saberma <mahb45@gmail.com>
2013-09-18 20:06:30 +0800

    add generate_batch_no utils.

--- lib/alipay/utils.rb
+++ lib/alipay/utils.rb
@@ -7,5 +7,12 @@ module Alipay
       end
       new_hash
     end
+
+    # 退款批次号，支付宝通过此批次号来防止重复退款操作，所以此号生成后最好直接保存至数据库，不要在显示页面的时候生成
+    # 共 24 位(8 位当前日期 + 9 位纳秒 + 1 位随机数)
+    def self.generate_batch_no
+      time = Time.now
+      time.strftime('%Y%m%d%H%M%S') + time.nsec.to_s + Random.new.rand(1..9).to_s
+    end
   end
 end
