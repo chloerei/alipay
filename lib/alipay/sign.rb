b4faa2a4a6552529679c1b98fc98945b015ec07f
Ben Hu <benzheren@gmail.com>
2013-10-31 17:26:19 +0800

    basic support for RSA sign

--- lib/alipay/sign.rb
+++ lib/alipay/sign.rb
@@ -2,20 +2,37 @@ require 'digest/md5'
 
 module Alipay
   module Sign
-    def self.generate(params)
+    MD5 = 1
+    RSA = 2
+
+    def self.generate_params(params)
       query = params.sort.map do |key, value|
         "#{key}=#{value}"
       end.join('&')
-
-      Digest::MD5.hexdigest("#{query}#{Alipay.key}")
     end
 
-    def self.verify?(params)
+    def self.generate(params, sign_type=MD5)
+      case sign_type
+      when MD5
+        Digest::MD5.hexdigest("#{generate_params(params)}#{Alipay.key}")
+      when RSA
+        Base64.encode64(Alipay.rsa_private_key.sign(
+          "sha1", params.force_encoding("utf-8")))
+      end
+    end
+
+    def self.verify?(params, sign_type=MD5)
       params = Utils.stringify_keys(params)
       params.delete('sign_type')
       sign = params.delete('sign')
 
-      generate(params) == sign
+      case sign_type
+      when MD5
+        generate(params) == sign
+      when RSA
+        Alipay.rsa_alipay_public_key.verify(
+          "sha1", Base64.decode64(sign), generate_params(params).force_encoding("utf-8"))
+      end
     end
   end
 end
