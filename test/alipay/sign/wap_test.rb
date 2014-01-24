014c982457d9daaa912aee87a677f3e81048eff3
HungYuHei <kongruxi@gmail.com>
2014-01-24 13:50:15 +0800

    Add Alipay::Sign::Wap.verify?

---
+++ test/alipay/sign/wap_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class Alipay::Sign::WapTest < Test::Unit::TestCase
+  def setup
+    @params = {
+      :v => '1.0',
+      :sec_id => 'MD5',
+      :service => 'test',
+      :notify_data => 'notify_data'
+    }
+    @sign = Digest::MD5.hexdigest("service=test&v=1.0&sec_id=MD5&notify_data=notify_data#{Alipay.key}")
+  end
+
+  def test_verify_sign
+    assert Alipay::Sign::Wap.verify?(@params.merge(:sign => @sign, :whatever => 'x'))
+  end
+end
