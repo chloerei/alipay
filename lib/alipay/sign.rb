77e3e6d54324a1d6ae7dc3591cda60816fc7c521
Rei <chloerei@gmail.com>
2015-03-23 14:58:31 +0800

    Move sign/wap code

--- lib/alipay/sign.rb
+++ lib/alipay/sign.rb
@@ -68,19 +68,5 @@ NG9zpgmLCUYuLkxpLQIDAQAB
 
       pkey.verify(digest, Base64.decode64(sign), params_to_string(params))
     end
-
-    module Wap
-      SORTED_VERIFY_PARAMS = %w( service v sec_id notify_data )
-
-      def self.verify?(params)
-        params = Utils.stringify_keys(params)
-
-        query = SORTED_VERIFY_PARAMS.map do |key|
-          "#{key}=#{params[key]}"
-        end.join('&')
-
-        params['sign'] == Digest::MD5.hexdigest("#{query}#{Alipay.key}")
-      end
-    end
   end
 end
