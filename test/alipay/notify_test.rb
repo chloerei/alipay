775ba4ba5c0f15e24a89c94609571e80d37c8d85
Rei <chloerei@gmail.com>
2015-03-28 15:29:10 +0800

    rename options to params in notify_test

--- test/alipay/notify_test.rb
+++ test/alipay/notify_test.rb
@@ -2,11 +2,11 @@ require 'test_helper'
 
 class Alipay::NotifyTest < Minitest::Test
   def setup
-    @options = {
+    @params = {
       :notify_id => '1234',
     }
-    @unsign_options = @options.merge(:sign_type => 'MD5', :sign => 'xxxx')
-    @sign_options = @options.merge(
+    @unsign_params = @params.merge(:sign_type => 'MD5', :sign => 'xxxx')
+    @sign_params = @params.merge(
       :sign_type => 'MD5',
       :sign      => '22fc7e38e5acdfede396aa463870d111'
     )
@@ -14,16 +14,16 @@ class Alipay::NotifyTest < Minitest::Test
 
   def test_unsign_notify
     FakeWeb.register_uri(:get, "https://mapi.alipay.com/gateway.do?service=notify_verify&partner=#{Alipay.pid}&notify_id=1234", :body => "true")
-    assert !Alipay::Notify.verify?(@unsign_options)
+    assert !Alipay::Notify.verify?(@unsign_params)
   end
 
   def test_verify_notify_when_true
     FakeWeb.register_uri(:get, "https://mapi.alipay.com/gateway.do?service=notify_verify&partner=#{Alipay.pid}&notify_id=1234", :body => "true")
-    assert Alipay::Notify.verify?(@sign_options)
+    assert Alipay::Notify.verify?(@sign_params)
   end
 
   def test_verify_notify_when_false
     FakeWeb.register_uri(:get, "https://mapi.alipay.com/gateway.do?service=notify_verify&partner=#{Alipay.pid}&notify_id=1234", :body => "false")
-    assert !Alipay::Notify.verify?(@sign_options)
+    assert !Alipay::Notify.verify?(@sign_params)
   end
 end
