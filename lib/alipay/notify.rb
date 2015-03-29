4b4c19ef8d112ecf64f22314a7ff7bc8c1dac9fc
Rei <chloerei@gmail.com>
2015-03-29 21:31:44 +0800

    add second arg options to notify verify? method

--- lib/alipay/notify.rb
+++ lib/alipay/notify.rb
@@ -1,12 +1,19 @@
 module Alipay
   module Notify
-    def self.verify?(params)
+    def self.verify?(params, options = {})
       params = Utils.stringify_keys(params)
-      Sign.verify?(params) && verify_notify_id?(params['notify_id'])
+      pid = options[:pid] || Alipay.pid
+      Sign.verify?(params, options) && verify_notify_id?(pid, params['notify_id'])
     end
 
-    def self.verify_notify_id?(notify_id)
-      Net::HTTP.get(URI("https://mapi.alipay.com/gateway.do?service=notify_verify&partner=#{Alipay.pid}&notify_id=#{CGI.escape(notify_id.to_s)}")) == 'true'
+    def self.verify_notify_id?(pid, notify_id)
+      uri = URI("https://mapi.alipay.com/gateway.do")
+      uri.query = URI.encode_www_form(
+        'service'   => 'notify_verify',
+        'partner'   => pid,
+        'notify_id' => notify_id
+      )
+      Net::HTTP.get(uri) == 'true'
     end
   end
 end
--- lib/alipay/notify/wap.rb
+++ lib/alipay/notify/wap.rb
@@ -1,11 +1,12 @@
 module Alipay
   module Notify
     module Wap
-      def self.verify?(params)
+      def self.verify?(params, options = {})
         params = Utils.stringify_keys(params)
+        pid = params[:pid] || Alipay.pid
         notify_id = params['notify_data'].scan(/\<notify_id\>(.*)\<\/notify_id\>/).flatten.first
 
-        Sign::Wap.verify?(params) && Notify.verify_notify_id?(notify_id)
+        Sign::Wap.verify?(params, options) && Notify.verify_notify_id?(pid, notify_id)
       end
     end
   end
