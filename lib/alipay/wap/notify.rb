bb4e271ab2f9ab4e2b1c9791c8deeb6f8722f535
Rei <chloerei@gmail.com>
2015-04-01 22:03:03 +0800

    Fix wap notify options

--- lib/alipay/wap/notify.rb
+++ lib/alipay/wap/notify.rb
@@ -3,7 +3,7 @@ module Alipay
     module Notify
       def self.verify?(params, options = {})
         params = Utils.stringify_keys(params)
-        pid = params[:pid] || Alipay.pid
+        pid = options[:pid] || Alipay.pid
         notify_id = params['notify_data'].scan(/\<notify_id\>(.*)\<\/notify_id\>/).flatten.first
 
         Sign.verify?(params, options) && ::Alipay::Notify.verify_notify_id?(pid, notify_id)
