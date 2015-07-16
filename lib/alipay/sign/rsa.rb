0983617ec325057eee0602b1c2c68b921d2afb9a
Sen <sen9ob@gmail.com>
2015-07-16 23:23:11 +0800

    base64 strict mode

--- lib/alipay/sign/rsa.rb
+++ lib/alipay/sign/rsa.rb
@@ -6,12 +6,12 @@ module Alipay
     class RSA
       def self.sign(key, string)
         rsa = OpenSSL::PKey::RSA.new(key)
-        Base64.encode64(rsa.sign('sha1', string))
+        Base64.strict_encode64(rsa.sign('sha1', string))
       end
 
       def self.verify?(key, string, sign)
         rsa = OpenSSL::PKey::RSA.new(key)
-        rsa.verify('sha1', Base64.decode64(sign), string)
+        rsa.verify('sha1', Base64.strict_decode64(sign), string)
       end
     end
   end
