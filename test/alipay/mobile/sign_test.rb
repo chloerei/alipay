498ca644fee2467fb8106d0806a93167f690c14d
Rei <chloerei@gmail.com>
2015-07-18 13:29:31 +0800

    Add Alipay::Mobile::Sign.params_to_string test

--- test/alipay/mobile/sign_test.rb
+++ test/alipay/mobile/sign_test.rb
@@ -1,4 +1,7 @@
 require 'test_helper'
 
 class Alipay::Mobile::SignTest < Minitest::Test
+  def test_params_to_string
+    assert_equal %q(a="1"&b="2"), Alipay::Mobile::Sign.params_to_string(a: 1, b: 2)
+  end
 end
