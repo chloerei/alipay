1103574d889d5a9183fb38eaf6d58c9aa3250069
Rei <chloerei@gmail.com>
2015-07-18 13:28:09 +0800

    Remove Alipay::Mobile::Sign.generate

--- lib/alipay/mobile/sign.rb
+++ lib/alipay/mobile/sign.rb
@@ -1,16 +1,6 @@
 module Alipay
   module Mobile
     module Sign
-      def self.generate(params, options = {})
-        params = Utils.stringify_keys(params)
-        sign_type = options[:sign_type] || Alipay.sign_type
-        key = options[:key] || Alipay.key
-        raise ArgumentError, "only support RSA sign_type" if sign_type != 'RSA'
-
-        string = params_to_string(params)
-        Alipay::Sign::RSA.sign(key, string)
-      end
-
       def self.params_to_string(params)
         params.map { |key, value| %Q(#{key}="#{value}") }.join('&')
       end
--- test/alipay/mobile/sign_test.rb
+++ test/alipay/mobile/sign_test.rb
@@ -1,14 +1,4 @@
 require 'test_helper'
 
 class Alipay::Mobile::SignTest < Minitest::Test
-  def test_generate
-    assert_equal 'DHEbGJCLa+EzoFTnSDyAerF04sMx/OBO6ObYVISlJqQnCyjGIVxn5JoEoiA82toq1m7VsxfQoTH0IMfN1dZJr0LgwXU3LFr1PvuTy2gyyuOUDTx5Mf5tEH/VfzdMnel8/JFJKffKwRiT+4AwWQ1TaOSIjpFAfI4jts3GVcK4+lE=',
-    Alipay::Mobile::Sign.generate({
-      service: 'test',
-      partner: '123'
-    }, {
-      sign_type: 'RSA',
-      key: TEST_RSA_PRIVATE_KEY
-    })
-  end
 end
