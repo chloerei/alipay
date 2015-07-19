f9d41c5272b4a059c194c9435d01a68249586e6f
Sen <sen9ob@gmail.com>
2015-07-20 00:28:03 +0800

    fix test

--- test/alipay/mobile/service_test.rb
+++ test/alipay/mobile/service_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class Alipay::Mobile::ServiceTest < Minitest::Test
   def test_mobile_securitypay_pay_string
-    assert_equal %q(service="mobile.securitypay.pay"&_input_charset="utf-8"&partner="1000000000000000"&seller_id="1000000000000000"&payment_type="1"&out_trade_no="1"&notify_url="/some_url"&subject="subject"&total_fee="0.01"&body="test"&sign="if1qjK4qnT7eQ5fw+BddHhO1LY6iuPY9Xmhkx81YKuCdceKWBdv798j+rxF9ZAhNW4Y3TMURm+XpgxhOh8lj8vorFup+MJ6fe2rXRWgxFhK9B8xuP2XH/3878g6d8Jq2D2gINTgDDL7+B5/IWWlwInas40cQTsVngG8mWkzB788="&sign_type="RSA"), Alipay::Mobile::Service.mobile_securitypay_pay_string({
+    assert_equal %q(service="mobile.securitypay.pay"&_input_charset="utf-8"&partner="1000000000000000"&seller_id="1000000000000000"&payment_type="1"&out_trade_no="1"&notify_url="/some_url"&subject="subject"&total_fee="0.01"&body="test"&sign="if1qjK4qnT7eQ5fw%2BBddHhO1LY6iuPY9Xmhkx81YKuCdceKWBdv798j%2BrxF9ZAhNW4Y3TMURm%2BXpgxhOh8lj8vorFup%2BMJ6fe2rXRWgxFhK9B8xuP2XH%2F3878g6d8Jq2D2gINTgDDL7%2BB5%2FIWWlwInas40cQTsVngG8mWkzB788%3D"&sign_type="RSA"), Alipay::Mobile::Service.mobile_securitypay_pay_string({
       out_trade_no: '1',
       notify_url: '/some_url',
       subject: 'subject',
