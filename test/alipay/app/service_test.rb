d96cbf4797841a3a26b0037481685f7c71f9dc0d
Rei <chloerei@gmail.com>
2017-06-11 17:42:30 +0800

    use static test data in app test

--- test/alipay/app/service_test.rb
+++ test/alipay/app/service_test.rb
@@ -2,39 +2,31 @@ require 'test_helper'
 
 class Alipay::Mobile::ServiceTest < Minitest::Test
   def setup
-    base_params = {
-      app_id: '2015052600090779',
-      biz_content: {:timeout_express=>"30m",:seller_id=>"",:product_code=>"QUICK_MSECURITY_PAY",:total_amount=>"0.01",:subject=>"1",:body=>"我是测试数据",:out_trade_no=>"IQJZSRC1YMQB5HU"}.to_json,
+    @params = {
+      app_id: '2016000000000000',
+      biz_content: {
+        timeout_express: "30m",
+        seller_id: "",
+        product_code: "QUICK_MSECURITY_PAY",
+        total_amount: "0.01",
+        subject: "1",
+        body: "测试数据",
+        out_trade_no: "10000000"
+      }.to_json,
       charset: 'utf-8',
       format: 'json',
       method: 'alipay.trade.app.pay',
-      notify_url: 'http://domain.merchant.com/payment_notify',
+      notify_url: 'http://example.com/notify',
       timestamp: '2016-08-25 20:26:31',
       version: '1.0'
     }
-    @rsa_sign_params  = base_params.merge(sign_type: 'RSA')
-    @rsa2_sign_params = base_params.merge(sign_type: 'RSA2')
-
-    @rsa_pkey  = OpenSSL::PKey::RSA.new(1024)
-    @rsa2_pkey = OpenSSL::PKey::RSA.new(2048)
-
-    @rsa_sign  = Base64.strict_encode64(@rsa_pkey .sign('sha1'  , @rsa_sign_params. sort.map{ |k,v| %Q(#{k}=#{v}) }.join('&')))
-    @rsa2_sign = Base64.strict_encode64(@rsa2_pkey.sign('sha256', @rsa2_sign_params.sort.map{ |k,v| %Q(#{k}=#{v}) }.join('&')))
   end
 
-  def test_mobile_securitypay_pay_string_with_rsa
-      encoded_str = Alipay::App::Service.alipay_trade_app_pay(@rsa_sign_params , sign_type: 'RSA' , key: @rsa_pkey.export)
-      params = Hash[ URI.decode_www_form(encoded_str).map {|(k,v)| [k.to_sym, v] } ]
-      sign = params.delete(:sign)
-      assert_equal sign, @rsa_sign
-      assert_equal params, @rsa_sign_params
+  def test_alipay_trade_app_pay_with_rsa
+      assert_equal 'app_id=2016000000000000&biz_content=%7B%22timeout_express%22%3A%2230m%22%2C%22seller_id%22%3A%22%22%2C%22product_code%22%3A%22QUICK_MSECURITY_PAY%22%2C%22total_amount%22%3A%220.01%22%2C%22subject%22%3A%221%22%2C%22body%22%3A%22%E6%B5%8B%E8%AF%95%E6%95%B0%E6%8D%AE%22%2C%22out_trade_no%22%3A%2210000000%22%7D&charset=utf-8&format=json&method=alipay.trade.app.pay&notify_url=http%3A%2F%2Fexample.com%2Fnotify&sign=Cvie7tAzUCH1JF7gVM2jMuEJ2%2FdHGWQesyl4MyV6MWnTeGzWhB4fgCJ%2B7EbdurHfiOVRpnGZkYc6twoWP53P2S6K4fp%2BazUJLVBgpSFy9ufn9Sf4BLpWMeOOEt5ufWdoWxWALhTbeEZnZReTDR1S%2FEXKw%2FTdjMWc3Q0kcG4ARzQ%3D&sign_type=RSA&timestamp=2016-08-25%2020%3A26%3A31&version=1.0', Alipay::App::Service.alipay_trade_app_pay(@params, sign_type: 'RSA' , key: TEST_RSA_PRIVATE_KEY)
   end
 
-  def test_mobile_securitypay_pay_string_with_rsa2
-      encoded_str = Alipay::App::Service.alipay_trade_app_pay(@rsa2_sign_params, sign_type: 'RSA2', key: @rsa2_pkey.export)
-      params = Hash[ URI.decode_www_form(encoded_str).map {|(k,v)| [k.to_sym, v] } ]
-      sign = params.delete(:sign)
-      assert_equal sign, @rsa2_sign
-      assert_equal params, @rsa2_sign_params
+  def test_alipay_trade_app_pay_with_rsa2
+      assert_equal 'app_id=2016000000000000&biz_content=%7B%22timeout_express%22%3A%2230m%22%2C%22seller_id%22%3A%22%22%2C%22product_code%22%3A%22QUICK_MSECURITY_PAY%22%2C%22total_amount%22%3A%220.01%22%2C%22subject%22%3A%221%22%2C%22body%22%3A%22%E6%B5%8B%E8%AF%95%E6%95%B0%E6%8D%AE%22%2C%22out_trade_no%22%3A%2210000000%22%7D&charset=utf-8&format=json&method=alipay.trade.app.pay&notify_url=http%3A%2F%2Fexample.com%2Fnotify&sign=fRBLfmUFXDRWW%2BtWD7BQlv8uymKU7Ky8TQKLF%2FnGkUsdCXnZ0MbqLFXAoE4Il6464n9YKncJhG50aDZ4s4HxpMMCku55so99nPkf3Uoksmje5Z5zSZusxnUHl5o7zf21%2BIOOf5mJzA%2Fp4BGZqFK00CoM07Ye9tSV%2Fid1hT8MbxA%3D&sign_type=RSA2&timestamp=2016-08-25%2020%3A26%3A31&version=1.0', Alipay::App::Service.alipay_trade_app_pay(@params, sign_type: 'RSA2' , key: TEST_RSA_PRIVATE_KEY)
   end
 end
--- test/alipay/app/sign_test.rb
+++ test/alipay/app/sign_test.rb
@@ -9,31 +9,29 @@ class Alipay::App::SignTest < Minitest::Test
       total_amount: '0.01'
     }
 
-    @rsa_pkey = OpenSSL::PKey::RSA.new(1024)
-    @rsa2_pkey = OpenSSL::PKey::RSA.new(2048)
   end
 
   def test_params_to_sorted_string
-    assert_equal %q(a=2&b=1), Alipay::App::Sign.params_to_sorted_string(b: 1, a: 2)
+    assert_equal 'a=2&b=1', Alipay::App::Sign.params_to_sorted_string(b: 1, a: 2)
   end
 
   def test_params_to_encoded_string
-    assert_equal %q(biz_content=%7B%22name%22%3A%22%E9%A1%BA%E9%81%93%22%7D&out_trade_no=MEM1234567&total_amount=0.01), Alipay::App::Sign.params_to_encoded_string(@params)
+    assert_equal 'biz_content=%7B%22name%22%3A%22%E9%A1%BA%E9%81%93%22%7D&out_trade_no=MEM1234567&total_amount=0.01', Alipay::App::Sign.params_to_encoded_string(@params)
   end
 
   def test_verify_with_rsa2
-    string = Alipay::App::Sign.params_to_sorted_string(@params)
-    sign = Base64.strict_encode64(@rsa2_pkey.sign('sha256', string))
-    params = @params.merge(sign: sign, sign_type: 'RSA2')
-
-    assert Alipay::App::Sign.verify?(params, sign_type: :rsa2, key: @rsa2_pkey.public_key.export)
+    sign = 'c1R7jXEzg/gMzCEwqBcrYf1EliSQEVyC2JaCt9AUkRqpxIQGeRAjA18gid3/ubFQn0vWC8ajNB0veyy7g7wlbi/gM/6S3qZpkLP5n+vgLG5v84IHVIHeQIf7a/U3olW2fFg9IonEr8ohIMYZD5IB89j+supMvtyPHhWHVal2N9k='
+    assert Alipay::App::Sign.verify?(@params.merge(sign: sign, sign_type: 'RSA2'),
+      sign_type: :rsa2,
+      key: TEST_RSA_PUBLIC_KEY
+    )
   end
 
   def test_verify_with_rsa
-    string = Alipay::App::Sign.params_to_sorted_string(@params)
-    sign = Base64.strict_encode64(@rsa_pkey.sign('sha1', string))
-    params = @params.merge(sign: sign, sign_type: 'RSA')
-
-    assert Alipay::App::Sign.verify?(params, sign_type: :rsa, key: @rsa_pkey.public_key.export)
+    sign = 'l+vXVjKLNqtCMAHDgaLdWZaUkjyKEfp85K3GzRx7HJnxdxHhF4MHh2AJdnzLdnp653BnGtPs8SBC/25D2edZdho0+6q+d8RJw0ZiDmZAg7WRP5rq7nB1/SBaH6hLgp8HXL6Uqwo42Rik3I4Ecw9u7uK1spt/Ph4vzZJBq5QMc9I='
+    assert Alipay::App::Sign.verify?(@params.merge(sign: sign, sign_type: 'RSA'),
+      sign_type: :rsa,
+      key: TEST_RSA_PUBLIC_KEY
+    )
   end
 end
