b64723337cfff64dbca849ac36d3c47139aa7c46
ryancheung <ryancheung.go@gmail.com>
2017-06-13 19:35:45 +0800

    Play well with old ruby version

--- test/alipay/open/service_test.rb
+++ test/alipay/open/service_test.rb
@@ -2,23 +2,23 @@ class Alipay::Open::ServiceTest < Minitest::Test
 
   def test_alipay_fund_trans_toaccount_transfer
     biz_content = {
-      'out_biz_no': 'OUT_BIZ_NO',
-      'payee_type': 'ALIPAY_LOGONID',
-      'payee_account': 'hi@example.com',
-      'amount': 10.00,
-      'payer_show_name': 'payer show namep',
-      'payee_real_name': 'real name',
-      'remark': 'remark'
+      'out_biz_no' => 'OUT_BIZ_NO',
+      'payee_type' => 'ALIPAY_LOGONID',
+      'payee_account' => 'hi@example.com',
+      'amount' => 10.00,
+      'payer_show_name' => 'payer show namep',
+      'payee_real_name' => 'real name',
+      'remark' => 'remark'
     }
 
     resp = {
-      "sign":"ERITJKEIJKJHKKKKKKKHJEREEEEEEEEEEE",
-      "alipay_fund_trans_toaccount_transfer_response":{
-        "pay_date":"2013-01-01 08:08:08",
-        "code":"10000",
-        "order_id":"20160627110070001502260006780837",
-        "msg":"Success",
-        "out_biz_no":"3142321423432"
+      "sign" => "ERITJKEIJKJHKKKKKKKHJEREEEEEEEEEEE",
+      "alipay_fund_trans_toaccount_transfer_response" => {
+        "pay_date" => "2013-01-01 08:08:08",
+        "code" => "10000",
+        "order_id" => "20160627110070001502260006780837",
+        "msg" => "Success",
+        "out_biz_no" => "3142321423432"
       }
     }.to_json
 
