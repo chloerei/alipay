8b88e439facd44b3f498a6dd42f46f23c48614bb
Rei <chloerei@gmail.com>
2017-06-25 20:39:47 +0800

    add Alipay::Client#request_url

--- lib/alipay/client.rb
+++ lib/alipay/client.rb
@@ -1,13 +1,39 @@
 module Alipay
   class Client
-    def initialize(url:, app_id:, app_private_key:, format: 'json', charset: 'utf-8', alipay_public_key:, sign_type: 'RSA2')
-      @url = url
-      @app_id = app_id
-      @app_private_key = app_private_key
-      @format = format
-      @charset = charset
-      @alipay_public_key = alipay_public_key
-      @sign_type = sign_type
+    def initialize(options)
+      options = ::Alipay::Utils.stringify_keys(options)
+      @url = options['url']
+      @app_id = options['app_id']
+      @app_private_key = options['app_private_key']
+      @format = options['format'] || 'json'
+      @charset = options['charset'] || 'utf-8'
+      @alipay_public_key = options['alipay_public_key']
+      @sign_type = options['sign_type'] || 'RSA2'
+    end
+
+    def request_url(options = {})
+      options = {
+        app_id: @app_id,
+        charset: @charset,
+        sign_type: @sign_type,
+        version: '1.0',
+        timestamp: Time.now.localtime('+08:00').strftime("%Y-%m-%d %H:%M:%S")
+      }.merge(options)
+
+      string = options.sort.map { |item| item.join('=') }.join('&')
+
+      sign = case @sign_type
+      when 'RSA'
+        ::Alipay::Sign::RSA.sign(@app_private_key, string)
+      when 'RSA2'
+        ::Alipay::Sign::RSA2.sign(@app_private_key, string)
+      else
+        raise "Unsupported sign_type: #{@sign_type}"
+      end
+
+      uri = URI(@url)
+      uri.query = URI.encode_www_form(options.merge(sign: sign))
+      uri.to_s
     end
   end
 end
--- test/alipay/client_test.rb
+++ test/alipay/client_test.rb
@@ -16,4 +16,8 @@ class Alipay::ClientTest < Minitest::Test
   def test_client_initialize
     refute_nil @client
   end
+
+  def test_request_url_for_alipay_trade_page_pay
+    assert_equal 'https://openapi.alipaydev.com/gateway.do?app_id=2016000000000000&charset=utf-8&sign_type=RSA2&version=1.0&timestamp=2016-04-01+00%3A00%3A00&method=alipay.trade.page.pay&biz_content=%7B%22out_trade_no%22%3A%2220160401000000%22%2C%22product_code%22%3A%22FAST_INSTANT_TRADE_PAY%22%2C%22total_amount%22%3A%220.01%22%2C%22subject%22%3A%22test%22%7D&sign=Dyx7%2BjfeGF4qgBHjJt6i2mJ9brI6uamBeq%2BH6SLe44CjtphTpBq5HUeuvGI7Dhar%2FzgfMP0tzBCwEjb%2FKdwG3ZdcyB6jOWzwi5YX3fVjLx%2FsFY5qr%2Fng%2BCuEsuZ5QId%2FLGQnp04cT%2BefayR20qfMwmAjQVowBBy3KsKb7iYdn6k%3D', @client.request_url(method: 'alipay.trade.page.pay', biz_content: { out_trade_no: '20160401000000', product_code: 'FAST_INSTANT_TRADE_PAY', total_amount: '0.01', subject: 'test' }.to_json, timestamp: '2016-04-01 00:00:00')
+  end
 end
